3f2dd65429f21f65c588e9c289881f424f5f21cf
alexrocampelo01 <alejandrorodriguezcampelo@gmail.com>
2026-05-02 20:27:09 +0200

    Mejoras en el front

--- Jugadores.js
+++ Jugadores.js
@@ -1,9 +1,25 @@
 let listaJugadores = [];
+let jugador ={
+    nombre: "juegador 1",
+    rol: "inocente",
+}
+/* Ejemplo de div que contiene a los jugadores
+<div class="lista">
+<div class="fila">
+  <div class="texto">eqg</div>
+  <div class="acciones">
+    <button class="btn borrar">Borrar</button>
+    <button class="btn editar">Editar</button>
+  </div>
+</div> 
+*/
 let nombreJugador = document.getElementById("nombreJugador");
 let butSumarJugador = document.getElementById("sumarJugador");
 let butCargarJugadores = document.getElementById("cargarLista")
 butSumarJugador.addEventListener("click", sumarJugador);
 butCargarJugadores.addEventListener("click", cargarJugadores);
+
+
 
 function sumarJugador(){
     // console.log(`nombre anadido ${nombreJugador.value}`);
@@ -18,15 +34,19 @@ function cargarJugadores(){
     let listaJugadoresHTML = document.getElementById('listaJugadores');
     console.log(listaJugadoresHTML)
     if(listaJugadores[0] != null){
+        listaJugadoresHTML.innerHTML="";
         console.log("datos");
         let listaNombre = document.createElement("li");
         listaNombre.textContent=listaJugadores[0];
         listaJugadoresHTML.appendChild(listaNombre);
+        listaNombre.addEventListener("click", modificarLista)
         console.log(listaNombre);
     }else{
         console.log("sin datos")
     }
+function modificarLista(e){
 
+}
 
     // for(let i = 0; i < listaJugadores.length-1; i++){
     //     console.log(`valor de i ${i}`);
